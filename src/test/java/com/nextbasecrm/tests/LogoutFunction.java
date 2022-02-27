d31e194baf8592a7b54ae5d2e92494ef26b3c83e
FarangisB <bayzaevafarangis@gmail.com>
2022-02-27 12:30:46 -0500

    Verified if the logout option is displayed

--- src/test/java/com/nextbasecrm/tests/LogoutFunction.java
+++ src/test/java/com/nextbasecrm/tests/LogoutFunction.java
@@ -3,8 +3,12 @@ package com.nextbasecrm.tests;
 import com.nextbasecrm.utilities.CRM_Utilities;
 import com.nextbasecrm.utilities.ConfigurationReader;
 import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,9 +21,27 @@ public class LogoutFunction {
 
         driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://login2.nextbasecrm.com");
-        CRM_Utilities.crm_login(driver, "hr28@cydeo.com", ConfigurationReader.getProperty("password"));
+        driver.get(ConfigurationReader.getProperty("env"));
+        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("HRUsername1"), ConfigurationReader.getProperty("password"));
 
+
+    }
+
+    @Test
+    public void logout_option_is_displayed(){
+
+        // 1. Users should access the homepage successfully
+
+        String expectedPageTitle = "Portal";
+        String actualPageTitle = driver.getTitle();
+
+        Assert.assertEquals(actualPageTitle, expectedPageTitle);
+
+        WebElement profileBtn = driver.findElement(By.xpath("//span[@class='user-name']"));
+        profileBtn.click();
+
+        WebElement logoutOption = driver.findElement(By.xpath("//span[.='Log out']"));
+        logoutOption.isDisplayed();
 
     }
 
