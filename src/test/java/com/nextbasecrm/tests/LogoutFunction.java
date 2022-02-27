848bd07c9c1e18064f936e2d7a401c13be523ee1
FarangisB <bayzaevafarangis@gmail.com>
2022-02-27 12:12:01 -0500

    Added setup method to LogoutFunction class

--- src/test/java/com/nextbasecrm/tests/LogoutFunction.java
+++ src/test/java/com/nextbasecrm/tests/LogoutFunction.java
@@ -1,7 +1,26 @@
 package com.nextbasecrm.tests;
 
+import com.nextbasecrm.utilities.CRM_Utilities;
+import com.nextbasecrm.utilities.ConfigurationReader;
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
 public class LogoutFunction {
 
+    WebDriver driver;
 
+    @BeforeMethod
+    public void setup_and_login(){
+
+        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://login2.nextbasecrm.com");
+        CRM_Utilities.crm_login(driver, "hr28@cydeo.com", ConfigurationReader.getProperty("password"));
+
+
+    }
 
 }
