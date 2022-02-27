543f87bb22359aa6c1d6b0468865cf4f2c9a4d6b
FarangisB <bayzaevafarangis@gmail.com>
2022-02-27 12:52:43 -0500

    Verified if users able to click logout option

--- src/test/java/com/nextbasecrm/tests/LogoutFunction.java
+++ src/test/java/com/nextbasecrm/tests/LogoutFunction.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -31,11 +32,14 @@ public class LogoutFunction {
     public void logout_option_is_displayed(){
 
         // 1. Users should access the homepage successfully
+        // Expected Title: "Portal"
 
         String expectedPageTitle = "Portal";
         String actualPageTitle = driver.getTitle();
 
         Assert.assertEquals(actualPageTitle, expectedPageTitle);
+
+        // 2. Users see the "Log out" option in the bottom of the dropdown
 
         WebElement profileBtn = driver.findElement(By.xpath("//span[@class='user-name']"));
         profileBtn.click();
@@ -44,5 +48,32 @@ public class LogoutFunction {
         logoutOption.isDisplayed();
 
     }
+
+    @Test
+    public void click_logout_option(){
+
+        // 1. Users should be able to access the homepage successfully
+        // 2. Users can see "Log out" option in the bottom of the dropdown
+        WebElement profileBtn = driver.findElement(By.xpath("//span[@class='user-name']"));
+        profileBtn.click();
+
+        // 3. Users should be able to go back to the Log in page successfully
+        // Expected Title: "Authorization"
+
+        WebElement logoutOption = driver.findElement(By.xpath("//span[.='Log out']"));
+        logoutOption.click();
+
+        String expectedPageTitle = "Authorization";
+        String actualPageTitle = driver.getTitle();
+
+        Assert.assertEquals(actualPageTitle, expectedPageTitle);
+
+    }
+
+    @AfterMethod
+    public void closeDriver(){
+        driver.close();
+    }
+
 
 }
