9cd9edaf2bdcd0d5ade8ea29eee0fcc0f85e422e
DenysTXJ <denyst89@gmail.com>
2022-03-02 13:05:48 -0500

    updated the code with some o the utilities used by the team runs good

--- src/test/java/com/nextbasecrm/tests/Group10UserStory11.java
+++ src/test/java/com/nextbasecrm/tests/Group10UserStory11.java
@@ -1,63 +1,47 @@
 package com.nextbasecrm.tests;
 
+import com.nextbasecrm.utilities.BrowserUtils;
+import com.nextbasecrm.utilities.CRM_Utilities;
+import com.nextbasecrm.utilities.ConfigurationReader;
 import com.nextbasecrm.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Group10UserStory11 {
 
     WebDriver driver;
 
+    @BeforeMethod
+    public void setup_and_login() {
+
+        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfigurationReader.getProperty("env"));
+        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("HRUsername1"), ConfigurationReader.getProperty("password"));
+    }
         @Test
         public void Test1(){
-            String crm1="https://login2.nextbasecrm.com/";
-            String crm2="https://login.nextbasecrm.com/";
-            String hr28="hr28@cydeo.com";
-            String hr29="hr29@cydeo.com";
-            String hr30="hr30@cydeo.com";
-            String mark28="marketing28@cydeo.com";
-            String mark29="marketing29@cydeo.com";
-            String mark30="marketing30@cydeo.com";
-            String hd28="helpdesk28@cydeo.com";
-            String hd29="helpdesk29@cydeo.com";
-            String hd30="helpdesk30@cydeo.com";
-            String password="UserUser";
-            ArrayList<String> userNames=new ArrayList<>();
-            userNames.add(hr28);
-            userNames.add(hr29);
-            userNames.add(hr30);
-            userNames.add(mark28);
-            userNames.add(mark29);
-            userNames.add(mark30);
-       /* userNames.add(hd28);
-        userNames.add(hd29);
-        userNames.add(hd30);*/
-            for (String userName : userNames) {
-                driver = WebDriverFactory.getDriver("chrome");
-                driver.manage().window().maximize();
-                driver.get(crm1);
-                WebElement loginInput = driver.findElement(By.name("USER_LOGIN"));
-                loginInput.sendKeys(userName);
-                WebElement passwordInput = driver.findElement(By.name("USER_PASSWORD"));
-                passwordInput.sendKeys(password);
-                WebElement loginButton = driver.findElement(By.className("login-btn"));
-                loginButton.click();
-                //user dropdown
+
+                //user dropdown to see the options, one of them is My Profile
                 WebElement userDropdown = driver.findElement(By.className("user-block"));
                 userDropdown.click();
+                //user selects My Profile option
                 WebElement myProfileOption = driver.findElement(By.className("menu-popup-item-text"));
                 myProfileOption.click();
-                //driver.close();
+
             }
-        }
-        @AfterMethod
-        public void wrapUp(){
-            driver.quit();
+
+    @AfterMethod
+    public void tearDown() {
+        BrowserUtils.sleep(3);
+        driver.close();
 
         }
 
